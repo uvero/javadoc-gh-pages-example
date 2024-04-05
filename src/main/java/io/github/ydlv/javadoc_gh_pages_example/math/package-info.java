6af5e040834583562b06680726c6513c1ccd4bc2
Yuval Dolev <uvero3@gmail.com>
2024-04-05 23:11:27 +0300

    :memo: Added two package-info.java files

---
+++ src/main/java/io/github/ydlv/javadoc_gh_pages_example/math/package-info.java
@@ -0,0 +1,26 @@
+/**
+ * This package contains some small implementations of some math algorithms
+ * and data structures. <br/>
+ * These should not be used for real world application, as this code
+ * is only meant for demonstrating Javadoc.
+ * <h1>Subpackages</h1>
+ * <ul>
+ *     <li>
+ *         {@link io.github.ydlv.javadoc_gh_pages_example.math.numeric_analysis} -
+ *          approximate solvers for finding roots of functions
+ *     </li>
+ *     <li>
+ *         {@link io.github.ydlv.javadoc_gh_pages_example.math.transformation} -
+ *         interfaces for mathematical functions
+ *     </li>
+ *     <li>
+ *         {@link io.github.ydlv.javadoc_gh_pages_example.math.cartesian_plane} -
+ *         mathematical objects on the Cartesian plane
+ *     </li>
+ *     <li>
+ *         {@link io.github.ydlv.javadoc_gh_pages_example.math.linalg} -
+ *         linear algebra
+ *     </li>
+ * </ul>
+ */
+package io.github.ydlv.javadoc_gh_pages_example.math;
