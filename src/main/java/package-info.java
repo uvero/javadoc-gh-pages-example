bc2b32705784e9fad62a44ac324d7c4e572db24e
Yuval Dolev <uvero3@gmail.com>
2024-04-06 00:00:33 +0300

    Added main package-info

---
+++ src/main/java/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This project is meant to demonstrate deploying Javadoc
+ * to GitHub Pages. It is not meant to be a production
+ * application. It is only meant to demonstrate how Javadoc
+ * can be deployed to GitHub Pages. The project contains implementation
+ * of some mathematical concepts.
+ */
